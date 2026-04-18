0fd689f2121dabb3774ab130c6185bd83bf44497
Suraj <144579945+Suraj6204@users.noreply.github.com>
2026-04-18 22:56:36 +0530

    Extend loading simulation duration to 2 seconds
    Increased the loading simulation duration from 1.5 seconds to 2 seconds.

--- src/components/notes/NotesContainer.tsx
+++ src/components/notes/NotesContainer.tsx
@@ -17,7 +17,7 @@ const NotesContainer = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1500);
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
